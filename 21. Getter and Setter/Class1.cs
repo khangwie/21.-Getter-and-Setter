9d54b54653437e7b04ef594533e91f70c88e0891
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-10 22:36:37 +0200

    Add project files.

---
+++ 21. Getter and Setter/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Getter_and_Setter
+{
+    internal class movie
+    {
+        public string Title;
+        public string Director;
+        private string rating;
+
+        public movie(string aTitle, string aDirector, string aRating)
+        {
+            Title = aTitle;
+            Director = aDirector;
+            Rating = aRating;
+        }
+
+        public string Rating
+        {
+            get { return Rating; }
+
+            set
+            {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R")
+                {
+                    Rating = value;
+
+                }
+                else
+                {
+                    Rating = "NR";
+                }
+            }
+        }
+    }
+}
+            
+        
+            
+        
+
+   
+
+                
+  
+        
+              
+
+        
+                
+
+
---
+++ 21. Getter and Setter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Getter_and_Setter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            movie movie1 = new movie("Titanic", "Jim", "PG-13");
+            movie movie2 = new movie("Unreleased", "Evans", "PG");
+
+            Console.WriteLine(movie1.Rating);
+            Console.ReadLine();
+        }
+    }
+}
